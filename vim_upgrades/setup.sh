f67aa78c68ffaab7b9bac32c29e8d9da9d05c927
Orion Collins <collinso@wit.edu>
2019-06-13 20:04:09 -0400

    added some more python support

---
+++ vim_upgrades/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "upgrading vim"
+cp vimrc ~/.vimrc
+sleep 1
+cp vimrc.plug ~/.vimrc.plug
+sleep 2
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim\
+echo 'Done!'
