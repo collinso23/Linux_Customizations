f6f1b102be615f08c2f1fcb4cf16933675177dca
Orion Collins <46937911+collinso23@users.noreply.github.com>
2019-10-12 15:11:20 -0400

    Update setup.sh
    removed error causing escape char in curl command '\'

--- vim_upgrades/setup.sh
+++ vim_upgrades/setup.sh
@@ -6,5 +6,5 @@ sleep 1
 cp vimrc.plug ~/.vimrc.plug
 sleep 2
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim\
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo 'Done!'
